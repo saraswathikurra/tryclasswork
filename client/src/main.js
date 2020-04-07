7aaf45d11295c1e9bee91b8cf35e449193626b6f
kurra <sarsukurra11@gmail.com>
2020-04-07 00:08:03 -0400

    imp

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bulma';
-import '@fortawesome/fontawesome-free/css/all.css'
+
 
 Vue.config.productionTip = false
 
