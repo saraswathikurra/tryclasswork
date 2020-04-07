f61a223e6a441f20cb196f696b9a152db6528f9f
kurra <sarsukurra11@gmail.com>
2020-04-06 23:33:07 -0400

    up to date

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Home from '../views/Home.vue';
+import Game from '../views/Game.vue';
+import Login from '../views/Login.vue';
+import { CurrentUser } from '../models/Users';
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/game', name: 'Game', component: Game, meta: { isSecret: true } },
+  { path: '/login', name: 'Login', component: Login },
+  
   {
     path: '/about',
     name: 'About',
@@ -24,6 +27,12 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach( (to, from, next) => {
+  if( to.meta.isSecret && !CurrentUser) next('/login');
+  else next();
+});
+
 
 export default router
